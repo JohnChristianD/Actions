d6c93d56080f37bdf229a7a7de3845dc10879d8b
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-06 18:46:23 +0800

    ci: close unary VJP primal transport

--- .ci/normalize-vjp-unary-products.py
+++ .ci/normalize-vjp-unary-products.py
@@ -2,29 +2,54 @@ from pathlib import Path
 
 path = Path('Exotic/ERL/FullCoupled/CompleteSafe_v147.agda')
 text = path.read_text()
-repls = {
-    'back px (c * dexp vx)': 'back px (Ring._*_ Rg c (SmoothAlgebra.dexp S vx))',
-    'back px (c * dlog vx)': 'back px (Ring._*_ Rg c (SmoothAlgebra.dlog S vx))',
-    'back px (c * dtanh vx)': 'back px (Ring._*_ Rg c (SmoothAlgebra.dtanh S vx))',
-    'back px (c * dsigmoid vx)': 'back px (Ring._*_ Rg c (SmoothAlgebra.dsigmoid S vx))',
-    'back px (c * dexp (eval x ρ))': 'back px (Ring._*_ Rg c (SmoothAlgebra.dexp S (eval x ρ)))',
-    'back px (c * dlog (eval x ρ))': 'back px (Ring._*_ Rg c (SmoothAlgebra.dlog S (eval x ρ)))',
-    'back px (c * dtanh (eval x ρ))': 'back px (Ring._*_ Rg c (SmoothAlgebra.dtanh S (eval x ρ)))',
-    'back px (c * dsigmoid (eval x ρ))': 'back px (Ring._*_ Rg c (SmoothAlgebra.dsigmoid S (eval x ρ)))',
-    '(c * dexp (eval x ρ))': '(Ring._*_ Rg c (SmoothAlgebra.dexp S (eval x ρ)))',
-    '(c * dlog (eval x ρ))': '(Ring._*_ Rg c (SmoothAlgebra.dlog S (eval x ρ)))',
-    '(c * dtanh (eval x ρ))': '(Ring._*_ Rg c (SmoothAlgebra.dtanh S (eval x ρ)))',
-    '(c * dsigmoid (eval x ρ))': '(Ring._*_ Rg c (SmoothAlgebra.dsigmoid S (eval x ρ)))',
-    'c * dexp (eval x ρ)': 'Ring._*_ Rg c (SmoothAlgebra.dexp S (eval x ρ))',
-    'c * dlog (eval x ρ)': 'Ring._*_ Rg c (SmoothAlgebra.dlog S (eval x ρ))',
-    'c * dtanh (eval x ρ)': 'Ring._*_ Rg c (SmoothAlgebra.dtanh S (eval x ρ))',
-    'c * dsigmoid (eval x ρ)': 'Ring._*_ Rg c (SmoothAlgebra.dsigmoid S (eval x ρ))',
-}
-changed = 0
-for old, new in repls.items():
-    n = text.count(old)
-    if n:
-        text = text.replace(old, new)
-        changed += n
+start_marker = '  vjpCoeff (expE x) ρ c i =\n'
+end_marker = '  data EState : Set where\n'
+start = text.find(start_marker)
+end = text.find(end_marker, start)
+if start < 0 or end < 0:
+    raise SystemExit('unary VJP theorem block not found')
+replacement = '''  vjpCoeff (expE x) ρ c i =
+    trans
+      (vjpCoeff x ρ (Ring._*_ Rg c (SmoothAlgebra.dexp S (Pullback.value (pull x ρ)))) i)
+      (trans
+        (cong
+          (λ d → Ring._*_ Rg d (coeff x ρ i))
+          (cong
+            (λ z → Ring._*_ Rg c (SmoothAlgebra.dexp S z))
+            (primalCorrect x ρ)))
+        (Ring.mulAssoc Rg c (SmoothAlgebra.dexp S (eval x ρ)) (coeff x ρ i)))
+  vjpCoeff (logE x) ρ c i =
+    trans
+      (vjpCoeff x ρ (Ring._*_ Rg c (SmoothAlgebra.dlog S (Pullback.value (pull x ρ)))) i)
+      (trans
+        (cong
+          (λ d → Ring._*_ Rg d (coeff x ρ i))
+          (cong
+            (λ z → Ring._*_ Rg c (SmoothAlgebra.dlog S z))
+            (primalCorrect x ρ)))
+        (Ring.mulAssoc Rg c (SmoothAlgebra.dlog S (eval x ρ)) (coeff x ρ i)))
+  vjpCoeff (tanhE x) ρ c i =
+    trans
+      (vjpCoeff x ρ (Ring._*_ Rg c (SmoothAlgebra.dtanh S (Pullback.value (pull x ρ)))) i)
+      (trans
+        (cong
+          (λ d → Ring._*_ Rg d (coeff x ρ i))
+          (cong
+            (λ z → Ring._*_ Rg c (SmoothAlgebra.dtanh S z))
+            (primalCorrect x ρ)))
+        (Ring.mulAssoc Rg c (SmoothAlgebra.dtanh S (eval x ρ)) (coeff x ρ i)))
+  vjpCoeff (sigmoidE x) ρ c i =
+    trans
+      (vjpCoeff x ρ (Ring._*_ Rg c (SmoothAlgebra.dsigmoid S (Pullback.value (pull x ρ)))) i)
+      (trans
+        (cong
+          (λ d → Ring._*_ Rg d (coeff x ρ i))
+          (cong
+            (λ z → Ring._*_ Rg c (SmoothAlgebra.dsigmoid S z))
+            (primalCorrect x ρ)))
+        (Ring.mulAssoc Rg c (SmoothAlgebra.dsigmoid S (eval x ρ)) (coeff x ρ i)))
+
+'''
+text = text[:start] + replacement + text[end:]
 path.write_text(text)
-print(f'vjp-unary-products-qualified={changed}')
+print('vjp-unary-products-transported-from-pull-value=True')
