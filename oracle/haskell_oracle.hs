e006e0ab5a430fff17bc66d45946554f081a090a
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-07 20:16:57 +0800

    Normalize Haskell oracle to LSTM-only five-column trace

--- oracle/haskell_oracle.hs
+++ oracle/haskell_oracle.hs
@@ -18,4 +18,4 @@ main :: IO ()
 main = mapM_ emit [(0.2,-0.1,0.3),(1.0,0.2,-0.4),(-0.7,0.5,0.1)]
   where
     emit (x,h,c) = let (lh,lc) = lstm x h c
-                    in printf "%.12f,%.12f,%.12f,%.12f\n" x h c lh lc
+                    in printf "%.12f,%.12f,%.12f,%.12f,%.12f\n" x h c lh lc
