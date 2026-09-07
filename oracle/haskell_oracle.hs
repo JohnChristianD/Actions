f13771fdf7a2db04332a613f2f94cafa33556245
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-07 17:05:35 +0800

    oracle: canonicalize Haskell numeric trace formatting

--- oracle/haskell_oracle.hs
+++ oracle/haskell_oracle.hs
@@ -13,7 +13,7 @@ lstmStep x h c =
   in o * tanh c'
 
 emit :: Double -> Double -> Double -> IO ()
-emit x h c = printf "%0.17g,%0.17g,%0.17g,%0.17g\n" x h c (lstmStep x h c)
+emit x h c = printf "%.17f,%.17f,%.17f,%.17f\n" x h c (lstmStep x h c)
 
 main :: IO ()
 main = do
