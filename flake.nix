63519f97fc9f570175e516dc458f42b4059efbcc
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-13 16:49:52 +0800

    build: make Nix discovery environment modular and portable

--- flake.nix
+++ flake.nix
@@ -8,14 +8,32 @@
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
     in {
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = [
             pkgs.git
+            pkgs.bash
+            pkgs.coreutils
+            pkgs.gnugrep
+            pkgs.gawk
             pkgs.haskellPackages.ghc
             pkgs.haskellPackages.cabal-install
           ];
+          shellHook = ''
+            export LC_ALL=C
+            export LANG=C
+          '';
         };
+      });
+
+      checks = forAllSystems (pkgs: {
+        discovery-toolchain = pkgs.runCommand "discovery-toolchain" { } ''
+          ${pkgs.haskellPackages.ghc}/bin/runghc --version > $out
+          ${pkgs.haskellPackages.cabal-install}/bin/cabal --version >> $out
+          ${pkgs.bash}/bin/bash --version >> $out
+        '';
       });
     };
 }
