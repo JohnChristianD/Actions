e6d11283d903fdc1fc5ac1f505541d178fa99d4b
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-19 07:54:39 +0800

    Simplify symbolic composition search generator

--- .ci/discovery/tsts_endogenous_discovery.m
+++ .ci/discovery/tsts_endogenous_discovery.m
@@ -24,14 +24,9 @@
 :- type program
     ---> theorem_program(theorem_schema, list(replacement_tag), int).
 
-:- func replacement_tags = list(replacement_tag).
-replacement_tags = [attention_tag, norm_tag, optimizer_tag].
-
-:- func theorem_schemas = list(theorem_schema).
-theorem_schemas =
-    [policy_replacement_composition,
-     norm_full_step_composition,
-     persistent_gru_full_step_composition].
+:- func one_step_sequences = list(list(replacement_tag)).
+one_step_sequences =
+    [[attention_tag], [norm_tag], [optimizer_tag]].
 
 :- func prepend_all(replacement_tag, list(list(replacement_tag)))
     = list(list(replacement_tag)).
@@ -39,24 +34,28 @@ prepend_all(_, []) = [].
 prepend_all(X, [Ys | Yss]) =
     [[X | Ys] | prepend_all(X, Yss)].
 
-:- func sequences_of_length(int) = list(list(replacement_tag)).
-sequences_of_length(0) = [[]].
-sequences_of_length(N) =
-    concat(map(prepend_all(replacement_tags), sequences_of_length(N - 1))).
+:- func all_sequences_of_length(int) = list(list(replacement_tag)).
+all_sequences_of_length(1) = one_step_sequences.
+all_sequences_of_length(2) =
+    prepend_all(attention_tag, one_step_sequences) ++
+    prepend_all(norm_tag, one_step_sequences) ++
+    prepend_all(optimizer_tag, one_step_sequences).
+all_sequences_of_length(3) =
+    prepend_all(attention_tag, all_sequences_of_length(2)) ++
+    prepend_all(norm_tag, all_sequences_of_length(2)) ++
+    prepend_all(optimizer_tag, all_sequences_of_length(2)).
 
 :- func policy_programs = list(program).
 policy_programs =
-    map(
-        (func(Rs) = theorem_program(policy_replacement_composition, Rs, 0)),
-        sequences_of_length(3))
-    ++
-    map(
-        (func(Rs) = theorem_program(policy_replacement_composition, Rs, 0)),
-        sequences_of_length(2))
-    ++
-    map(
-        (func(Rs) = theorem_program(policy_replacement_composition, Rs, 0)),
-        sequences_of_length(1)).
+    map_policy(all_sequences_of_length(3)) ++
+    map_policy(all_sequences_of_length(2)) ++
+    map_policy(all_sequences_of_length(1)).
+
+:- func map_policy(list(list(replacement_tag))) = list(program).
+map_policy([]) = [].
+map_policy([Rs | Rss]) =
+    [theorem_program(policy_replacement_composition, Rs, 0) |
+     map_policy(Rss)].
 
 :- func preservation_programs = list(program).
 preservation_programs =
@@ -76,20 +75,13 @@ tag_name(attention_tag) = "attention".
 tag_name(norm_tag) = "norm".
 tag_name(optimizer_tag) = "optimizer".
 
-:- func schema_name(theorem_schema) = string.
-schema_name(policy_replacement_composition) = "policy_replacement_composition".
-schema_name(norm_full_step_composition) = "norm_full_step_composition".
-schema_name(persistent_gru_full_step_composition) =
-    "persistent_gru_full_step_composition".
-
 :- func seq_name(list(replacement_tag)) = string.
 seq_name([]) = "empty".
-seq_name([X | Xs]) =
-    foldl(
-        (func(T, Acc) =
-            (if Acc = "" then tag_name(T) else Acc ++ "_" ++ tag_name(T))),
-        [X | Xs],
-        "").
+seq_name([X | Xs]) = tag_name(X) ++ seq_name_tail(Xs).
+
+:- func seq_name_tail(list(replacement_tag)) = string.
+seq_name_tail([]) = "".
+seq_name_tail([X | Xs]) = "_" ++ tag_name(X) ++ seq_name_tail(Xs).
 
 :- func nat_string(int) = string.
 nat_string(N) = string.int_to_string(N).
@@ -100,16 +92,13 @@ parameter_binders(Tags) = parameter_binders_from(Tags, 1).
 :- func parameter_binders_from(list(replacement_tag), int) = string.
 parameter_binders_from([], _) = "".
 parameter_binders_from([attention_tag | Ts], I) =
-    " (a" ++ nat_string(I) ++
-    " : C.LearnedSparsemaxAttention)" ++
+    " (a" ++ nat_string(I) ++ " : C.LearnedSparsemaxAttention)" ++
     parameter_binders_from(Ts, I + 1).
 parameter_binders_from([norm_tag | Ts], I) =
-    " (n" ++ nat_string(I) ++
-    " : C.NormPair)" ++
+    " (n" ++ nat_string(I) ++ " : C.NormPair)" ++
     parameter_binders_from(Ts, I + 1).
 parameter_binders_from([optimizer_tag | Ts], I) =
-    " (o" ++ nat_string(I) ++
-    " : C.F4IntUState)" ++
+    " (o" ++ nat_string(I) ++ " : C.F4IntUState)" ++
     parameter_binders_from(Ts, I + 1).
 
 :- func parameter_pattern(list(replacement_tag)) = string.
@@ -133,84 +122,57 @@ replacement_expr(optimizer_tag, I) =
     "C.optimizerReplacement o" ++ nat_string(I).
 
 :- func replacement_list_expr(list(replacement_tag)) = string.
-replacement_list_expr(Tags) =
-    replacement_list_expr_from(Tags, 1).
+replacement_list_expr(Tags) = replacement_list_expr_from(Tags, 1).
 
 :- func replacement_list_expr_from(list(replacement_tag), int) = string.
 replacement_list_expr_from([], _) = "[]".
 replacement_list_expr_from([T | Ts], I) =
     replacement_expr(T, I) ++
-    " C.∷ " ++ replacement_list_expr_from(Ts, I + 1).
+    " ∷ " ++
+    replacement_list_expr_from(Ts, I + 1).
 
 :- func policy_candidate_name(list(replacement_tag)) = string.
-policy_candidate_name(Tags) =
-    "candidate_policy_" ++ seq_name(Tags).
+policy_candidate_name(Tags) = "candidate_policy_" ++ seq_name(Tags).
 
 :- func render_policy_candidate(list(replacement_tag)) = string.
 render_policy_candidate(Tags) =
-    policy_candidate_name(Tags) ++ " :
-" ++
-    "  ∀ K s" ++ parameter_binders(Tags) ++ " →
-" ++
-    "  C.canonicalPolicy K
-" ++
-    "    (C.applyLearnerReplacements
-" ++
-    "      (" ++ replacement_list_expr(Tags) ++ ")
-" ++
-    "      s)
-" ++
-    "  ≡
-" ++
-    "  C.canonicalPolicy K s
-" ++
-    policy_candidate_name(Tags) ++ parameter_pattern(Tags) ++ " =
-" ++
-    "  C.canonicalPolicy-learnerReplacement-composition
-" ++
-    "    K s (" ++ replacement_list_expr(Tags) ++ ")
-
-".
+    policy_candidate_name(Tags) ++ " :" ++
+    "\n  ∀ K s" ++ parameter_binders(Tags) ++ " →" ++
+    "\n  C.canonicalPolicy K" ++
+    "\n    (C.applyLearnerReplacements" ++
+    "\n      (" ++ replacement_list_expr(Tags) ++ ")" ++
+    "\n      s)" ++
+    "\n  ≡" ++
+    "\n  C.canonicalPolicy K s" ++
+    "\n" ++
+    policy_candidate_name(Tags) ++ parameter_pattern(Tags) ++ " =" ++
+    "\n  C.canonicalPolicy-learnerReplacement-composition" ++
+    "\n    K s (" ++ replacement_list_expr(Tags) ++ ")" ++
+    "\n\n".
 
 :- func render_preservation_candidate(theorem_schema, int) = string.
 render_preservation_candidate(norm_full_step_composition, N) =
-    "candidate_norm_full_step_" ++ nat_string(N) ++ " :
-" ++
-    "  ∀ K s →
-" ++
-    "  C.normPairWeightPlusOne
-" ++
-    "    (C.norm (C.iterateCanonical K " ++ nat_string(N) ++ " s))
-" ++
-    "  ≡
-" ++
-    "  C.normPairWeightPlusOne (C.norm s)
-" ++
-    "candidate_norm_full_step_" ++ nat_string(N) ++ " K s =
-" ++
-    "  C.canonicalNormPair-afterFullStep-iterate K " ++
-    nat_string(N) ++ " s
-
-".
+    "candidate_norm_full_step_" ++ nat_string(N) ++ " :" ++
+    "\n  ∀ K s →" ++
+    "\n  C.normPairWeightPlusOne" ++
+    "\n    (C.norm (C.iterateCanonical K " ++ nat_string(N) ++ " s))" ++
+    "\n  ≡" ++
+    "\n  C.normPairWeightPlusOne (C.norm s)" ++
+    "\ncandidate_norm_full_step_" ++ nat_string(N) ++ " K s =" ++
+    "\n  C.canonicalNormPair-afterFullStep-iterate K " ++
+    nat_string(N) ++ " s" ++
+    "\n\n".
 render_preservation_candidate(persistent_gru_full_step_composition, N) =
-    "candidate_persistent_gru_full_step_" ++ nat_string(N) ++ " :
-" ++
-    "  ∀ K s →
-" ++
-    "  C.persistentGRU
-" ++
-    "    (C.gru (C.iterateCanonical K " ++ nat_string(N) ++ " s))
-" ++
-    "  ≡
-" ++
-    "  C.persistentGRU (C.gru s)
-" ++
-    "candidate_persistent_gru_full_step_" ++ nat_string(N) ++ " K s =
-" ++
-    "  C.canonicalPersistentGRU-afterFullStep-iterate K " ++
-    nat_string(N) ++ " s
-
-".
+    "candidate_persistent_gru_full_step_" ++ nat_string(N) ++ " :" ++
+    "\n  ∀ K s →" ++
+    "\n  C.persistentGRU" ++
+    "\n    (C.gru (C.iterateCanonical K " ++ nat_string(N) ++ " s))" ++
+    "\n  ≡" ++
+    "\n  C.persistentGRU (C.gru s)" ++
+    "\ncandidate_persistent_gru_full_step_" ++ nat_string(N) ++ " K s =" ++
+    "\n  C.canonicalPersistentGRU-afterFullStep-iterate K " ++
+    nat_string(N) ++ " s" ++
+    "\n\n".
 render_preservation_candidate(_, _) = "".
 
 :- func render_program(program) = string.
@@ -220,11 +182,6 @@ render_program(theorem_program(S, [], N)) =
     render_preservation_candidate(S, N).
 render_program(theorem_program(_, _, _)) = "".
 
-:- func candidate_names(list(program)) = list(string).
-candidate_names([]) = [].
-candidate_names([P | Ps]) =
-    [program_name(P) | candidate_names(Ps)].
-
 :- func program_name(program) = string.
 program_name(theorem_program(policy_replacement_composition, Tags, _)) =
     policy_candidate_name(Tags).
@@ -232,6 +189,13 @@ program_name(theorem_program(norm_full_step_composition, _, N)) =
     "candidate_norm_full_step_" ++ nat_string(N).
 program_name(theorem_program(persistent_gru_full_step_composition, _, N)) =
     "candidate_persistent_gru_full_step_" ++ nat_string(N).
+
+:- pred write_programs(io.text_output_stream::in, list(program)::in,
+    io::di, io::uo) is det.
+write_programs(_, [], !IO).
+write_programs(Stream, [P | Ps], !IO) :-
+    io.write_string(Stream, render_program(P), !IO),
+    write_programs(Stream, Ps, !IO).
 
 :- pred write_agda_program(list(program)::in, io::di, io::uo) is det.
 write_agda_program(Programs, !IO) :-
@@ -241,6 +205,7 @@ write_agda_program(Programs, !IO) :-
         io.write_string(Stream,
             "{-# OPTIONS --safe #-}\n\n" ++
             "module GeneratedLearnerCompositionDiscovery where\n\n" ++
+            "open import Data.List.Base using (List; []; _∷_)\n" ++
             "open import Exotic.ERL.FullCoupled.CanonicalLearnerMonolith as C\n" ++
             "open import Exotic.ERL.FullCoupled.TheoremsMonolith\n\n",
             !IO),
@@ -254,39 +219,35 @@ write_agda_program(Programs, !IO) :-
         io.set_exit_status(1, !IO)
     ).
 
-:- pred write_programs(io.text_output_stream::in, list(program)::in,
+:- pred write_names(io.text_output_stream::in, list(string)::in,
     io::di, io::uo) is det.
-write_programs(_, [], !IO).
-write_programs(Stream, [P | Ps], !IO) :-
-    io.write_string(Stream, render_program(P), !IO),
-    write_programs(Stream, Ps, !IO).
+write_names(_, [], !IO).
+write_names(Stream, [N | Ns], !IO) :-
+    io.write_string(Stream, "    \"" ++ N ++ "\"", !IO),
+    (
+        Ns = [] ->
+            io.write_string(Stream, "\n", !IO)
+    ;
+            io.write_string(Stream, ",\n", !IO)
+    ),
+    write_names(Stream, Ns, !IO).
 
 :- pred write_report(list(program)::in, io::di, io::uo) is det.
 write_report(Programs, !IO) :-
     io.open_output("tsts-endogenous-composition-candidate.json", Result, !IO),
     (
         Result = ok(Stream),
-        io.write_string(Stream, "{\n", !IO),
-        io.write_string(Stream, "  \"accepted\": true,\n", !IO),
         io.write_string(Stream,
+            "{\n" ++
+            "  \"accepted\": true,\n" ++
             "  \"search_semantics\": " ++
-            "\"TSTS-ordered symbolic search over typed learner theorem compositions\",\n",
-            !IO),
-        io.write_string(Stream,
-            "  \"proof_gate\": \"GeneratedLearnerCompositionDiscovery.agda\",\n",
-            !IO),
-        io.write_string(Stream,
+            "\"TSTS-ordered symbolic search over typed learner theorem compositions\",\n" ++
+            "  \"proof_gate\": \"GeneratedLearnerCompositionDiscovery.agda\",\n" ++
             "  \"candidate_count\": " ++
-            nat_string(list.length(Programs)) ++ ",\n",
-            !IO),
-        io.write_string(Stream,
-            "  \"candidate_names\": [\n",
-            !IO),
-        write_names(Stream, candidate_names(Programs), !IO),
-        io.write_string(Stream,
-            "  ],\n" ++
+            nat_string(list.length(Programs)) ++ ",\n" ++
             "  \"selection_metric\": \"unspecified by semantic contract\",\n" ++
-            "  \"external_equivalence\": false\n}\n",
+            "  \"external_equivalence\": false\n" ++
+            "}\n",
             !IO),
         io.close_output(Stream)
     ;
@@ -296,17 +257,6 @@ write_report(Programs, !IO) :-
             !IO),
         io.set_exit_status(1, !IO)
     ).
-
-:- pred write_names(io.text_output_stream::in, list(string)::in,
-    io::di, io::uo) is det.
-write_names(_, [], !IO).
-write_names(Stream, [N | Ns], !IO) :-
-    io.write_string(Stream, "    \"" ++ N ++ "\"", !IO),
-    (
-        Ns = [] -> io.write_string(Stream, "\n", !IO)
-    ;   io.write_string(Stream, ",\n", !IO)
-    ),
-    write_names(Stream, Ns, !IO).
 
 main(!IO) :-
     Programs = programs,
