dfadde963687a200224740bc7ffa1d78860ff70f
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-19 10:42:41 +0800

    Normalize Mercury e-graph module syntax

--- .ci/discovery/interpolated_theorem_egraph.m
+++ .ci/discovery/interpolated_theorem_egraph.m
@@ -2,7 +2,7 @@
 
 :- interface.
 
- :- import_module io.
+:- import_module io.
 :- import_module symbolic_egraph.
 
 :- type theorem_goal
@@ -34,7 +34,7 @@
 :- func saturated_goal_count = int.
 :- pred main(io::di, io::uo) is det.
 
- :- implementation.
+:- implementation.
 
 :- import_module int.
 :- import_module list.
