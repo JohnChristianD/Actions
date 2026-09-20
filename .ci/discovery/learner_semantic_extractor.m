fa97f48429e99a5cccbec061d8a63a637b546c08
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-20 22:54:06 +0800

    fix Mercury identifier-boundary character parsing

--- .ci/discovery/learner_semantic_extractor.m
+++ .ci/discovery/learner_semantic_extractor.m
@@ -138,7 +138,7 @@ identifier_char(C) :-
     ;
     C = '_'
     ;
-    C = '-'.
+    char.to_int(C) = 45.
 
 :- pred occurrence_boundary(string::in, string::in, int::in) is semidet.
 occurrence_boundary(Text, Name, Position) :-
