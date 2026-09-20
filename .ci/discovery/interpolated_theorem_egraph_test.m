9ba430fda43362bf6091914b290174deb8d6f32e
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-20 22:51:33 +0800

    retarget interpolated e-graph regression to canonical theorem

--- .ci/discovery/interpolated_theorem_egraph_test.m
+++ .ci/discovery/interpolated_theorem_egraph_test.m
@@ -8,11 +8,10 @@
 
 :- implementation.
 
-:- import_module io.
-:- import_module symbolic_egraph.
+:- import_module list.
 :- import_module interpolated_theorem_egraph.
 :- import_module learner_semantic_manifest.
-:- import_module list.
+:- import_module symbolic_egraph.
 
 main(!IO) :-
     read_manifest(Laws, !IO),
@@ -21,23 +20,13 @@ main(!IO) :-
     analyze(E, Analyses),
     add_expr(app("semantic-law", [atom(forced_target_id)]), E, TargetClass, E1),
     extract_best(TargetClass, E1, 64, _, ExtractionCost),
-    CompositeCount = list.length(
-        list.filter(
-            (pred(L::in) is semidet :-
-                semantic_law.composite(L) = yes),
-            Laws)),
-    ExactUAPId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#ExactUniversalApproximationThroughContinuousLeftInverse",
-    ContinuousBoundaryId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalNoGlobalInt8ContinuousLeftInverseOnDiscreteTopologies",
-    InjectivityId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#leftInverse-observation-injective",
-    FiniteFeatureId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalNoGlobalFiniteFeatureContinuousLeftInverseOnDiscreteTopologies",
-    FiniteTimeId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalFiniteTimeExactUniversalReadout",
-    FiniteSampleId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalFiniteSampleExactUniversalReadout",
-    IterateCompositionId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalIterateComposition",
-    MixingPrefixCompositionId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#CanonicalHadamardAttentionRopePrefixCompositionTheorem",
-    AperiodicityId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalAperiodic-theorem",
-    FiniteCycleId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalNoNontrivialFiniteCycle-theorem",
-    VisitId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#finiteStateActionVisitInjectionImpossible",
-    EmergentExactOrbitId = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#CanonicalFiniteExactOrbitUAPCompositionTheorem",
+    TargetId = forced_target_id,
+    PolicyAttention = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalPolicy-attention-invariant",
+    PolicyNorm = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalPolicy-norm-invariant",
+    PolicyOptimizer = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalPolicy-optimizer-invariant",
+    HardSparse = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#hardSparse-composition-normPair-F4-L2",
+    Recurrent = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalGRU-recurrent-associative-scan-theorem",
+    HardSign = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#hardSignGate-idempotent",
     NonReflexive = list.length(
         list.filter(
             (pred(L::in) is semidet :-
@@ -49,8 +38,17 @@ main(!IO) :-
                 semantic_law.source(L) =
                     "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda"),
             Laws) = Laws,
-        CompositeCount > 0,
-        NonReflexive >= CompositeCount,
+        list.member(TargetLaw, Laws),
+        law_id(TargetLaw) = TargetId,
+        semantic_law.composite(TargetLaw) = yes,
+        list.member(PolicyAttention, semantic_law.dependencies(TargetLaw)),
+        list.member(PolicyNorm, semantic_law.dependencies(TargetLaw)),
+        list.member(PolicyOptimizer, semantic_law.dependencies(TargetLaw)),
+        list.member(HardSparse, semantic_law.dependencies(TargetLaw)),
+        list.member(Recurrent, semantic_law.dependencies(TargetLaw)),
+        list.member(HardSignLaw, Laws),
+        law_id(HardSignLaw) = HardSign,
+        NonReflexive > 0,
         QuotientCount > 0,
         class_count(E) > 0,
         enode_count(E) > 0,
@@ -58,30 +56,6 @@ main(!IO) :-
         list.length(Analyses) > 0,
         saturation_iterations(Saturation) > 0,
         ExtractionCost > 0,
-        list.member(ExactUAPLaw, Laws),
-        law_id(ExactUAPLaw) = ExactUAPId,
-        list.member(ContinuousBoundaryLaw, Laws),
-        law_id(ContinuousBoundaryLaw) = ContinuousBoundaryId,
-        list.member(InjectivityLaw, Laws),
-        law_id(InjectivityLaw) = InjectivityId,
-        list.member(FiniteFeatureLaw, Laws),
-        law_id(FiniteFeatureLaw) = FiniteFeatureId,
-        list.member(FiniteTimeLaw, Laws),
-        law_id(FiniteTimeLaw) = FiniteTimeId,
-        list.member(FiniteSampleLaw, Laws),
-        law_id(FiniteSampleLaw) = FiniteSampleId,
-        list.member(IterateCompositionLaw, Laws),
-        law_id(IterateCompositionLaw) = IterateCompositionId,
-        list.member(MixingPrefixCompositionLaw, Laws),
-        law_id(MixingPrefixCompositionLaw) = MixingPrefixCompositionId,
-        list.member(AperiodicityLaw, Laws),
-        law_id(AperiodicityLaw) = AperiodicityId,
-        list.member(FiniteCycleLaw, Laws),
-        law_id(FiniteCycleLaw) = FiniteCycleId,
-        list.member(VisitLaw, Laws),
-        law_id(VisitLaw) = VisitId,
-        list.member(EmergentExactOrbitLaw, Laws),
-        law_id(EmergentExactOrbitLaw) = EmergentExactOrbitId,
         e_match(
             papp("proof-compose", [
                 pvar("A"),
@@ -97,7 +71,7 @@ main(!IO) :-
             "quotient=proof-compose-associativity "
             "e-matching=on saturation=on rebuild=on "
             "eclass-analysis=on cost-extraction=on "
-            "dynamic=on\n",
+            "target=polymorphic-sparsemax\n",
             !IO)
     ;
         io.write_string(
