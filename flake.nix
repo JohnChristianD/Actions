330d47ad9f6683b9e0a7a48e9e8fb113c6341c4e
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-20 09:34:22 +0800

    ci: migrate flake.nix

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Pinned Nix environment for the Agda kernel and Mercury e-graph lanes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/29c6bca3b9a3ee1263483043c0e50321eb4ec7ae";
+
+    agda-src = {
+      url = "github:agda/agda/v2.8.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    agda-stdlib = {
+      url = "github:agda/agda-stdlib/v2.4";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, agda-src, agda-stdlib }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      mkPkgs = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ agda-src.overlays.default ];
+        };
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = mkPkgs system;
+
+          standardLibrary = pkgs.agdaPackages.standard-library.overrideAttrs
+            (_old: {
+              version = "2.4";
+              src = agda-stdlib;
+            });
+
+          agdaWithStandardLibrary =
+            pkgs.agdaPackages.agda.withPackages [ standardLibrary ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              agdaWithStandardLibrary
+              pkgs.mercury
+              pkgs.gnumake
+              pkgs.git
+            ];
+
+            shellHook = ''
+              printf 'agda=%s\n' "$(agda --version | head -n 1)"
+              printf 'mercury=%s\n' "$(mmc --version | head -n 1)"
+            '';
+          };
+        });
+    };
+}
