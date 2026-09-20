8d35113f3d634abe86de69fb5bfa933756692a76
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-20 09:43:55 +0800

    ci: use cached Nixpkgs Agda package

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/29c6bca3b9a3ee1263483043c0e50321eb4ec7ae";
 
-    agda-src = {
-      url = "github:agda/agda/v2.8.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     agda-stdlib = {
       url = "github:agda/agda-stdlib/v2.4";
@@ -15,7 +11,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, agda-src, agda-stdlib }:
+  outputs = { self, nixpkgs, agda-stdlib }:
     let
       systems = [
         "x86_64-linux"
@@ -27,10 +23,7 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
 
       mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ agda-src.overlays.default ];
-        };
+        import nixpkgs { inherit system; };
     in
     {
       devShells = forAllSystems (system:
