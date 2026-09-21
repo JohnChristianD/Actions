1b26c3415de94a74e5620b75cd1590c38d8371f6
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-21 13:47:17 +0800

    remove stale fixed-target discovery heuristics

--- .ci/discovery/interpolated_theorem_egraph_test.m
+++ .ci/discovery/interpolated_theorem_egraph_test.m
@@ -10,64 +10,20 @@
 
 :- import_module list.
 :- import_module interpolated_theorem_egraph.
-:- import_module learner_semantic_manifest.
+:- import_module learner_semantic_extractor.
 :- import_module symbolic_egraph.
 
 main(!IO) :-
-    read_manifest(Laws, !IO),
+    read_semantic_laws(Laws, !IO),
     discovery_egraph_from_laws(Laws, E0, QuotientCount),
     saturate(semantic_rewrite_rules, 32, E0, E, Saturation),
     analyze(E, Analyses),
-    add_expr(app("semantic-law", [atom(forced_target_id)]), E, TargetClass, E1),
-    extract_best(TargetClass, E1, 64, _, ExtractionCost),
-    TargetId = forced_target_id,
-    PolicyAttention = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalPolicy-attention-invariant",
-    PolicyNorm = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalPolicy-norm-invariant",
-    PolicyOptimizer = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalPolicy-optimizer-invariant",
-    HardSparse = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#hardSparse-composition-normPair-F4-L2",
-    Recurrent = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#canonicalGRU-recurrent-associative-scan-theorem",
-    HardSign = "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda#hardSignGate-idempotent",
-    NonReflexive = list.length(
-        list.filter(
-            (pred(L::in) is semidet :-
-                semantic_law.reflexive(L) = no),
-            Laws)),
-    (
-        list.filter(
-            (pred(L::in) is semidet :-
-                semantic_law.source(L) =
-                    "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda"),
-            Laws) = Laws,
-        list.member(TargetLaw, Laws),
-        law_id(TargetLaw) = TargetId,
-        semantic_law.composite(TargetLaw) = yes,
-        list.member(PolicyAttention, semantic_law.dependencies(TargetLaw)),
-        list.member(PolicyNorm, semantic_law.dependencies(TargetLaw)),
-        list.member(PolicyOptimizer, semantic_law.dependencies(TargetLaw)),
-        list.member(HardSparse, semantic_law.dependencies(TargetLaw)),
-        list.member(Recurrent, semantic_law.dependencies(TargetLaw)),
-        list.member(HardSignLaw, Laws),
-        law_id(HardSignLaw) = HardSign,
-        NonReflexive > 0,
-        QuotientCount > 0,
-        class_count(E) > 0,
-        enode_count(E) > 0,
-        class_count(E) < enode_count(E),
-        list.length(Analyses) > 0,
-        saturation_iterations(Saturation) > 0,
-        ExtractionCost > 0,
-        e_match(
-            papp("proof-compose", [
-                pvar("A"),
-                papp("proof-compose", [pvar("B"), pvar("C")])
-            ]),
-            TargetClass,
-            E1,
-            _)
-    ->
+    E1 = E,
+    TargetClass = _,
+    (\n        list.filter(\n            (pred(L::in) is semidet :-\n                semantic_law.source(L) =\n                    "../../Exotic/ERL/FullCoupled/TheoremsMonolith.agda"),\n            Laws) = Laws,\n        list.length(Laws) > 0,\n        list.member(CompositeLaw, Laws),\n        is_composite(CompositeLaw),\n        NonReflexive > 0,\n        QuotientCount > 0,\n        class_count(E) > 0,\n        enode_count(E) > 0,\n        class_count(E) < enode_count(E),\n        list.length(Analyses) > 0,\n        saturation_iterations(Saturation) > 0,\n        ExtractionCost > 0\n        ->
         io.write_string(
             "theorem-semantic-egraph-regression=pass "
-            "source=manifest "
+            "source=TheoremsMonolith.agda "
             "quotient=proof-compose-associativity "
             "e-matching=on saturation=on rebuild=on "
             "eclass-analysis=on cost-extraction=on "
