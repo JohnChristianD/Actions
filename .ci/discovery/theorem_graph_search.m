c17527dd87b3b2adef496ffc07f796a7741f5f07
John Christian Domingo <26460879+JohnChristianD@users.noreply.github.com>
2026-09-22 23:34:17 +0800

    Place restored graph plan definitions in implementation section

--- .ci/discovery/theorem_graph_search.m
+++ .ci/discovery/theorem_graph_search.m
@@ -72,6 +72,43 @@
 
 :- implementation.
 
+:- pred graph_finite_observation_stationary_limit_plan(
+    list(semantic_law)::in,
+    list(string)::out) is semidet.
+graph_finite_observation_stationary_limit_plan(Laws, Plan) :-
+    search_named_required_plan(
+        "FiniteObservationStationaryLimitTheorem",
+        Laws,
+        Plan).
+
+:- pred graph_finite_observation_stationary_plan(
+    list(semantic_law)::in,
+    list(string)::out) is semidet.
+graph_finite_observation_stationary_plan(Laws, Plan) :-
+    search_named_required_plan(
+        "CanonicalFiniteObservationStationarySubcompositionTheorem",
+        Laws,
+        Plan).
+
+:- pred graph_endogenous_observation_plan(
+    list(semantic_law)::in,
+    list(string)::out) is semidet.
+graph_endogenous_observation_plan(Laws, Plan) :-
+    search_named_required_plan(
+        "CanonicalEndogenousObservationBoundaryTheorem",
+        Laws,
+        Plan).
+
+:- pred graph_endogenous_rnnlm_pomdp_observation_topology_plan(
+    list(semantic_law)::in,
+    list(string)::out) is semidet.
+graph_endogenous_rnnlm_pomdp_observation_topology_plan(Laws, Plan) :-
+    search_named_required_plan(
+        "CanonicalEndogenousRNNLMPOMDPObservationTopologyCapabilityTheorem",
+        Laws,
+        Plan).
+
+
 graph_finite_probability_mass_plan(Laws, Plan) :-
     search_named_required_plan(
         "FiniteProbabilityMassSemanticsTheorem",
@@ -518,42 +555,6 @@ graph_required_subcompositions = [
     "CanonicalExactRNNLMCapabilitySubcompositionTheorem",
     "CanonicalExactRNNLMObservationSubcompositionTheorem"
 ].
-
-:- pred graph_finite_observation_stationary_limit_plan(
-    list(semantic_law)::in,
-    list(string)::out) is semidet.
-graph_finite_observation_stationary_limit_plan(Laws, Plan) :-
-    search_named_required_plan(
-        "FiniteObservationStationaryLimitTheorem",
-        Laws,
-        Plan).
-
-:- pred graph_finite_observation_stationary_plan(
-    list(semantic_law)::in,
-    list(string)::out) is semidet.
-graph_finite_observation_stationary_plan(Laws, Plan) :-
-    search_named_required_plan(
-        "CanonicalFiniteObservationStationarySubcompositionTheorem",
-        Laws,
-        Plan).
-
-:- pred graph_endogenous_observation_plan(
-    list(semantic_law)::in,
-    list(string)::out) is semidet.
-graph_endogenous_observation_plan(Laws, Plan) :-
-    search_named_required_plan(
-        "CanonicalEndogenousObservationBoundaryTheorem",
-        Laws,
-        Plan).
-
-:- pred graph_endogenous_rnnlm_pomdp_observation_topology_plan(
-    list(semantic_law)::in,
-    list(string)::out) is semidet.
-graph_endogenous_rnnlm_pomdp_observation_topology_plan(Laws, Plan) :-
-    search_named_required_plan(
-        "CanonicalEndogenousRNNLMPOMDPObservationTopologyCapabilityTheorem",
-        Laws,
-        Plan).
 
 :- pred graph_search_completion(
     list(semantic_law)::in,
